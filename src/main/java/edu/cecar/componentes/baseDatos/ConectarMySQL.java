bf5852a4213e69c2ba9f6b46764dda6b4c3590a9
JuanBenavides96 <48834673+JuanBenavides96@users.noreply.github.com>
2019-08-26 10:59:45 -0500

    Update ConectarMySQL.java

--- src/main/java/edu/cecar/componentes/baseDatos/ConectarMySQL.java
+++ src/main/java/edu/cecar/componentes/baseDatos/ConectarMySQL.java
@@ -22,12 +22,16 @@ public class ConectarMySQL {
 			
 			// Se crea un URL hacia la maquina y la base de datos
 			String url= "jdbc:mysql://" + servidorBD + "/" + nombreBD + "?useUnicode=true&useJDBCCompliantTimezoneShift=true&useLegacyDatetimeCode=false&serverTimezone=UTC"; 
-
+                      
+		       //connection=DriverManager.getConnection(url,usuario,password);
+			
+			
+			
 			//se crea la conexion a la base de datos
 			connection=DriverManager.getConnection(url,usuario,password);
 
 		}catch  (Exception e) {
-
+                       
 			System.out.println("Error"+e);
 		}
 	}
